f04d521ee39cbf3fd1891a2b24defa9287b1a9a8
athusx <110572717+athusx@users.noreply.github.com>
2022-08-18 21:55:44 -0300

    Add files via upload

---
+++ aa.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class aa{
+    public static void main(String[] args);
+    char pa;
+    Scanner ler = new Scanner(System.in);
+    
+    System.out.print("Digite uma palavra com 4 letras: ")
+    pa = ler.nextLine;
+
+
+
+
+
+
+}
